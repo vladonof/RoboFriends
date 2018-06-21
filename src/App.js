6daa16528a826b5c7d061ce99c534968994ad3eb
vlad-alexandru.onofrei@student.manchester.ac.uk <vlad.onofrei96@gmail.com>
2018-06-22 00:43:36 +0100

    improved functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots'
 import SearchBox from './SearchBox';
 import './App.css'; 
 
@@ -8,10 +7,16 @@ class App extends Component {
 	constructor(){
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}; 
 	}
+
+  componentDidMount(){
+  	fetch('https://jsonplaceholder.typicode.com/users')
+  	.then(response => response.json())
+  	.then(users => this.setState({robots: users}));
+  }
 	onSearchChange = (event) => { 
 		this.setState({ searchfield: event.target.value })
 	}
