8bf8fb01a27a5123a2aa0144ec0bc60dfa5d92a0
vlad-alexandru.onofrei@student.manchester.ac.uk <vlad.onofrei96@gmail.com>
2018-06-21 22:47:21 +0100

    added robot card

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import robots from './robots'
 
-ReactDOM.render(<Hello greeting={`Hello React Ninja `} / >, document.getElementById('root'));
+ReactDOM.render(<Card / >, document.getElementById('root'));
 registerServiceWorker();
