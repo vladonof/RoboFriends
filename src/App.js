c21ae9b681d87873e3e2709575eb5e07d58ffcc5
vlad-alexandru.onofrei@student.manchester.ac.uk <vlad.onofrei96@gmail.com>
2018-06-22 00:55:04 +0100

    added scroll component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css'; 
 
 class App extends Component {
@@ -14,7 +15,7 @@ class App extends Component {
 
   componentDidMount(){
   	fetch('https://jsonplaceholder.typicode.com/users')
-  	.then(response => response.json())
+  	.then(response =>  response.json())
   	.then(users => this.setState({robots: users}));
   }
 	onSearchChange = (event) => { 
@@ -28,7 +29,9 @@ class App extends Component {
 			<div className = "tc">
 				<h1 className = "f1">RoboFriends</h1>
 				<SearchBox searchChange={ this.onSearchChange }/>
-				<CardList robots={ filteredRobots } />
+				<Scroll>
+					<CardList robots={ filteredRobots } />
+				</Scroll>
 			</div>
 		)
 	}
